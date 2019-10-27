b02ae491468ed4987106de185b00c336a9d68619
sanjayLaptop <ayushbhatt3.ab@gmail.com>
2019-10-27 19:04:48 +0530

    adding the login with google authentication

--- src/app/bs-navbar/bd-navbar.component.ts
+++ src/app/bs-navbar/bd-navbar.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bd-navbar.component.html',
   styleUrls: ['./bd-navbar.component.css']
 })
-export class BdNavbarComponent implements OnInit {
+export class BdNavbarComponent{
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) { }
 
-  ngOnInit() {
+  logout() {
+    this.afAuth.auth.signOut();
   }
 
 }
